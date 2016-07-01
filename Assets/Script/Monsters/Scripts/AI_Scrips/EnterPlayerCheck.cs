6129c0d7f7ed391701ba62a9e7cc196247abdee6
dds4120 <dds4120@naver.com>
2016-07-01 17:35:16 +0900

    2016.07.01 AI 제작
    boss ai create

---
+++ Assets/Script/Monsters/Scripts/AI_Scrips/EnterPlayerCheck.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterPlayerCheck : MonoBehaviour
+{
+
+	//field
+	private bool isEntered = false;
+
+	//property
+	public bool Entered {
+		get {
+			return isEntered;
+		}
+	}
+
+	//check entered object
+	public void OnTriggerStay (Collider col)
+	{
+		if (col.gameObject.CompareTag ("Player"))
+			isEntered = true;
+	
+	}
+
+	//exit object
+	public void OnTriggerExit (Collider col)
+	{
+		if (col.gameObject.CompareTag ("Player"))
+			isEntered = false;
+		
+	}
+}
