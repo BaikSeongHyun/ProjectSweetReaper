3c15f4cddc4d38414bbc05078dced92628f5a1c5
unknown <red96146@naver.com>
2016-07-19 16:56:49 +0900

    개구리 달리기.

--- Assets/Pet/NPCFrogPet.cs
+++ Assets/Pet/NPCFrogPet.cs
@@ -6,24 +6,26 @@ public class NPCFrogPet : Pet
 
 	public Animator NPCFrogPetAiAnimator;
 
-
-
+	AnimatorStateInfo runStateNPCPetFrog;
 
 	public enum NPCFrogPetPatternName
 	{
 		NPCFrogIdle = 1,
-		NPCFrogRun,
-		NPCFrogSlow,
-		NPCFrogAttack,
-		NPCFrogHitDamege,
-		NPCFrogWin,
-		NPCFrogLose
+		NPCFrogRun,//2
+		NPCFrogSlow,//3
+		NPCFrogAttack,//4
+		NPCFrogHitDamege,//5
+		NPCFrogWin,//6
+		NPCFrogLose//7
 	};
 
 	// Use this for initialization
 	void Start () 
 	{
 		NPCFrogPetAiAnimator = GetComponent<Animator> ();
+//		peaceImage.gameObject.SetActive (false);
+//		violenceImage.gameObject.SetActive (false);
+		NPCFrogPetPattern(NPCFrogPetPatternName.NPCFrogIdle);
 
 
 	}
@@ -31,8 +33,79 @@ public class NPCFrogPet : Pet
 	// Update is called once per frame
 	void Update () 
 	{
-		transform.position = new Vector3 (0, 0, 0);
-		
+		//NPCFrogPetRandomPatternCycle += Time.deltaTime;
+
+		NPCFrogPetPattern (NPCFrogPetPatternName.NPCFrogRun);
+
+		if (runStateNPCPetFrog.IsName ("NPCFrogRun"))
+		{
+			petRunningSpeed = 4.0f;
+			//transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z+ petRunningSpeed);
+			transform.Translate(transform.forward * (Time.deltaTime* petRunningSpeed));	
+		}
+
+		runStateNPCPetFrog = this.NPCFrogPetAiAnimator.GetCurrentAnimatorStateInfo( 0 );
+
+
+//		if (NPCFrogPetRandomPatternCycle <= 5)
+//		{
+//			NPCFrogPetRandomPattern = Random.Range (0,2);
+//		
+//
+//			if (NPCFrogPetRandomPattern == 0)
+//			{
+//				NPCFrogPetPattern (NPCFrogPetPatternName.NPCFrogSlow);
+//				petRunningSpeed = 0.0f;
+//
+//				transform.position = new Vector3 (transform.position.x, transform.position.y, transform.position.z + petRunningSpeed);
+//	
+//			} 
+//			else if (NPCFrogPetRandomPattern == 2)
+//			{
+//				Debug.Log ("Attack");
+//			}
+//
+//			NPCFrogPetRandomPatternCycle = 0;
+//
+//		}
+//
+
 	
 	}
+
+
+	public void NPCFrogPetPattern( NPCFrogPetPatternName NPCFrogState)
+	{
+
+		switch (NPCFrogState) 
+		{
+		case NPCFrogPetPatternName.NPCFrogIdle:
+			NPCFrogPetAiAnimator.SetInteger ("NPCFrogState", 1);
+			break;
+		
+		case NPCFrogPetPatternName.NPCFrogRun:
+			NPCFrogPetAiAnimator.SetInteger ("NPCFrogState", 2);
+			break;
+		
+		case NPCFrogPetPatternName.NPCFrogSlow:
+			NPCFrogPetAiAnimator.SetInteger ("NPCFrogState", 3);
+			break;
+		
+		case NPCFrogPetPatternName.NPCFrogAttack:
+			NPCFrogPetAiAnimator.SetInteger ("NPCFrogState", 4);
+			break;
+		case NPCFrogPetPatternName.NPCFrogHitDamege:
+			NPCFrogPetAiAnimator.SetInteger ("NPCFrogState", 5);
+			break;
+
+		case NPCFrogPetPatternName.NPCFrogWin:
+			NPCFrogPetAiAnimator.SetInteger ("NPCFrogState", 6);
+			break;
+
+		case NPCFrogPetPatternName.NPCFrogLose:
+			NPCFrogPetAiAnimator.SetInteger ("NPCFrogState", 7);
+			break;
+		}
+	}
+
 }
