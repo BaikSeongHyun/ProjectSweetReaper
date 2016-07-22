19e35025651dd53c0a328d7fa06deab3232cc478
unknown <red96146@naver.com>
2016-07-22 17:59:23 +0900

    병합

---
+++ Assets/Pet/RaceStageManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class RaceStageManager : MonoBehaviour {
+
+	const int petCount = 6;
+	public GameObject petType;
+	public List<Pet> petList;
+	public Pet myPet;
+	public UserInterfaceManager mainUI;
+
+	// Use this for initialization
+	void Start () 
+	{
+		EveryPet ();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		mainUI.UpdateMainUI ();			
+	}
+
+
+
+
+
+	public void EveryPet()
+	{
+		petList = new List<Pet> ();
+		GameObject[] temp = GameObject.FindGameObjectsWithTag ("Pet");
+
+		for (int i = 0; i < temp.Length; i++) 
+		{
+			petList.Add( temp [i].GetComponent<Pet> ());
+		}
+
+		for (int i = 0; i < temp.Length; i++) 
+		{
+			if (petList [i].playerPet) {
+				myPet = petList [i];
+				petList.RemoveAt (i);
+			}
+		}
+	
+	}
+
+	public void PetPlacement()
+	{
+		int randomPlacement = Random.Range (0, 5);
+
+		//pet[0] pet[randomPlacement]
+									
+	}
+
+	public void OrderMyPet(string data)
+	{
+		
+		if (data == " Run")
+			myPet.UserOrder ("Run");
+	}
+
+
+}
