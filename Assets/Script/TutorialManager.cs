b9dc14bbb59ab9366547bd52f5362c00c99e7669
sjk98027 <kakrkddusdn@naver.com>
2016-07-25 17:59:25 +0900

    2016 07 25 튜토리얼.... 멍...(해야할거 : gamecontroler 대신할 튜토리얼매니저 제작, 튜토리얼 UI새로 제작-파야에 연결)

--- Assets/Script/TutorialManager.cs
+++ Assets/Script/TutorialManager.cs
@@ -1,59 +1,54 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class TutorialManager : MonoBehaviour {
-//	GameController controller;
-	// Use this for initialization
-	public GameObject ExplainText1;
-	public GameObject ExplainText2;
-	public GameObject ExplainText3;
-	public GameObject ExplainText4;
-	public GameObject ExplainText5;
-	public GameObject ExplainText6;
-	public GameObject ExplainText7;
+//	GameController controller; 대신쓴다
+
+	public Vector3 cameraDistance;
+	public GameObject temp;
+
+	//complex data field
+	public CharacterFaye faye;
+	public CharacterInformation info;
+	public UserInterfaceManager mainUI;
+	public DataBase dataBase;
 
 
 
-	void Start () {
-		LinkElement ();
-		CloseElement ();
-		StartTutorial ();
+
+
+	// initialize this script
+	void Start()
+	{
+		Application.targetFrameRate = 80;
+		faye = GameObject.FindWithTag( "Player" ).GetComponent<CharacterFaye>();
+		mainUI = GameObject.FindWithTag( "MainUI" ).GetComponent<UserInterfaceManager>();
+		cameraDistance = new Vector3 ( 0f, 7.5f, -8f );
 	}
+
+
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-	void StartTutorial()
-	{
-		float startTime = Time.realtimeSinceStartup;
-		if (startTime >= 1f) {
-			ExplainText1.SetActive (true);
-		}
-	}
 
 
-	public void LinkElement()
-	{
-		ExplainText1 = GameObject.Find ("ExplainText1");
-		ExplainText2 = GameObject.Find ("ExplainText2");
-		ExplainText3 = GameObject.Find ("ExplainText3");
-		ExplainText4 = GameObject.Find ("ExplainText4");
-		ExplainText5 = GameObject.Find ("ExplainText5");
-		ExplainText6 = GameObject.Find ("ExplainText6");
-		ExplainText7 = GameObject.Find ("ExplainText7");
-	}
-	public void CloseElement()
-	{
-		ExplainText1.SetActive (false);
-		ExplainText2.SetActive (false);
-		ExplainText3.SetActive (false);
-		ExplainText4.SetActive (false);
-		ExplainText5.SetActive (false);
-		ExplainText6.SetActive (false);
-		ExplainText7.SetActive (false);
-	}
+
+
+//	delegate void StartTutorial();
+//
+//	event StartTutorial advancement;
+//
+
+
+
+
+	//public void ()
 
 
 }
