34eb4e64986e20718601900470c62e69546539b2
unknown <red96146@naver.com>
2016-07-28 18:34:20 +0900

    2016.07.28 누락 스크립트 추가

---
+++ Assets/Script/UIScript/RaceUI/RaceMiniMap.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RaceMiniMap : MonoBehaviour
+{
+	public Scrollbar minimapBar;
+
+	public void LinkElement()
+	{
+		minimapBar = transform.Find("Scrollbar").GetComponent<Scrollbar>();
+	}
+
+	public void UpdateMinimap(float information)
+	{
+		minimapBar.value = information;
+	}
+}
